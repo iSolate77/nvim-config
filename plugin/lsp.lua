be462b63af27f946517759f7cfa29354a74e0b43
Mohamed Faris <mohd.faris07@gmail.com>
2025-02-09 12:58:22 +0300

    Add more lsps and remove deprecated

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,5 +1,7 @@
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1, float = true }) end,
+	{ desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end,
+	{ desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>k", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
@@ -40,13 +42,14 @@ vim.diagnostic.config({
 		focusable = true,
 		style = "minimal",
 		border = "rounded",
-		source = "always",
+		source = 'if_many',
 		header = "",
 		prefix = "",
 	},
 })
 
 vim.lsp.enable({
-	'lua_ls',
+	'luals',
 	'gopls',
+	'clangd',
 })
